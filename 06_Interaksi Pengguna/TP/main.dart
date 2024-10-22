8f8b1dc0133b675d3d0edbab62779292e81691d5
dwicandrapratama4@gmail.com <DCandraPratama>
2024-10-22 20:31:24 +0700

    Interaksi Pengguna

---
+++ 06_Interaksi Pengguna/TP/main.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Latihan BottomNavigationBar',
+      theme: ThemeData(
+        primarySwatch: Colors.orange,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _selectedIndex = 0;
+
+  // List of pages to display based on selected index
+  static const List<Widget> _pages = <Widget>[
+    Center(child: Text('Ini Halaman Beranda')),
+    Center(child: Text('Ini Halaman Wisata')),
+    Center(child: Text('Ini Halaman Profile')),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Latihan BottomNavigationBar'),
+        backgroundColor: Colors.orange,
+      ),
+      body: _pages[_selectedIndex], // Displaying the corresponding page
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Beranda',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.business),
+            label: 'Wisata',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber[800],
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
